ce43606b613393b2fdf384b188f1163a6985a8cb
ivanavocode <ivanavocode@gmail.com>
2019-10-11 00:27:12 +0600

    first commit

---
+++ precache-manifest.675247a6b0035036fd8dec476b027c32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8413272a7813cb44b6ac5d424aaadb3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "caf3714e764c7c381f75",
+    "url": "./static/css/main.0c27c052.chunk.css"
+  },
+  {
+    "revision": "4eda8b0169927d23d037",
+    "url": "./static/js/2.3508c049.chunk.js"
+  },
+  {
+    "revision": "caf3714e764c7c381f75",
+    "url": "./static/js/main.0bc32195.chunk.js"
+  },
+  {
+    "revision": "8f38ab79102242f44994",
+    "url": "./static/js/runtime~main.4670cfa6.js"
+  }
+]);
